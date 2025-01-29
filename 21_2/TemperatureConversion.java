66eadd319e6e08214b9f31a688cc225529f21bcc
piyush0528 <140838165+piyush0528@users.noreply.github.com>
2025-01-29 22:27:11 +0530

    Delete 21_2 directory

--- 21_2/TemperatureConversion.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class TemperatureConversion{
-public static void main(String [] args){
-Scanner sc=new Scanner(System.in);
-int farenheit=sc.nextInt();
-int celsiusResult= (farenheit-32) * (5/9) ;
-System.out.println("The "+farenheit+" farenheit is "+celsiusResult+" Celsius");
-}
-}
